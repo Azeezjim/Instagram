0c8e314d41e32e055487410b9eaad93de06a1365
Azeezjim <azeexjim@gmail.com>
2022-03-09 03:09:04 -1100

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import FirebaseContext from "./context/firebase"
+import FirebaseContext from "./context/firebase";
+import { firebase, FieldValue } from './lib/firebase';
 
 
 ReactDOM.render(
     
-  <FirebaseContext.Provider value={{alufirebase, FieldValue}}>
+  <FirebaseContext.Provider value={{firebase, FieldValue}}>
     <App />
   </FirebaseContext.Provider>,
     document.getElementById('root')
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,27 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { seedDatabase } from '../servicdes/seed';
+// import { seedDatabase } from '../servicdes/seed';
 
 // import seed file
+import { seedDatabase } from '../seed';
 
 
-const config = {};
-
+const config = {
+  apiKey: "AIzaSyAimucIqJ0jI70SDGh8XGkyKk1ZhoVmFq0",
+  authDomain: "instagram-c92f7.firebaseapp.com",
+  projectId: "instagram-c92f7",
+  storageBucket: "instagram-c92f7.appspot.com",
+  messagingSenderId: "540612807007",
+  appId: "1:540612807007:web:25c3370332417f96118eea"
+};
+  
 const firebase =Firebase.initializeApp(config);
-
 const {FieldValue} = Firebase.firestore;
 
 // call seed file only once 
 // seedDatabase(firebase)
+// seedDatabase(firebase)
+console.log("firebase", firebase);
+
+export {firebase, FieldValue}
