6a2b5613cfcabdd743658f702ac8f9ec5b8d8bd9
Azeezjim <azeexjim@gmail.com>
2022-06-04 03:50:17 -1100

    latest commit of the day

--- src/pages/dashBoard.js
+++ src/pages/dashBoard.js
@@ -14,7 +14,7 @@ export default function Dashboard () {
   return(
     <div className="bg-gray-background">
       <Header />
-      <div className="grid grid-cols-3 gap-4 justify-between">
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
         <Timeline />
         <Sidebar />
       </div>
