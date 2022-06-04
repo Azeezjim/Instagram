b2fe4e1a0fc7d2ef202b500cfcb64ef6025a1056
Azeezjim <azeexjim@gmail.com>
2022-06-04 06:22:49 -1100

    latest commit of the day

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,11 @@
+import useUser from '../hooks/use-user'
+
 export default function Sidebar() {
   return (
+
+    comnst x = useUser()
+    console.log("x", x);
     <p>I i'm the sidebar </p>
   )
 }  
+
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,10 +6,24 @@ export async function doseUsernameExist(userName) {
     .collection('users')
     .where('username', '==', userName.toLowerCase())
     .get()
-    console.log('result', result.docs.map((user) => user.length > 0));
+    // console.log('result', result.docs.map((user) => user.length > 0));
 
     return result.docs.map((user) => user.length > 0); 
     // return result.docs.length > 0;
 
+}
+
+export async function getUserByUserId(userId) {
+  const result = await firebase 
+  .firestore()
+    .collection('users')
+    .where('userId', '==', userId.toLowerCase())
+    .get()
+
+    const user = result.docs.map((item) => ({
+      ...item.data(),
+      docId: item.id
+    }))
+    return user;
 }
 
