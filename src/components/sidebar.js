9b924468f9239fecd0eda2c65b5520f57182de2c
Azeezjim <azeexjim@gmail.com>
2022-06-04 23:06:17 -1100

    latest commit of the day

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,14 @@
 import useUser from '../hooks/use-user'
 
 export default function Sidebar() {
-  return ( 
+
 
     const {
       user: {fullName, usernmae, userId}
     } = useUser()
 
-    return <div className='gray'>
+    return (
+    <div className='gray'>
       <User />
     <Suggestions />
     </div>
