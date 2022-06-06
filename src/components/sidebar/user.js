30cf8f23dc201a6f0d4b43cb87dbb89f28ad063b
Azeezjim <azeexjim@gmail.com>
2022-06-06 00:34:14 -1100

    latest commit of the day

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,5 +1,14 @@
-export default function User (){
+import PropTypes  from "prop-types"
+import { Link } from 'react-router-dom'
+import Skeleton from "react-loading-skeleton"
+
+export default function User (username, fullName){
   return (
     <p>I am a User</p>
   )
+}
+
+User.propTypes ={
+  username: PropTypes.string,
+  fullname: PropTypes.string
 }
