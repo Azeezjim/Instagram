3cda580558722a0c8fd4864c40174ebca2858a57
Azeezjim <azeexjim@gmail.com>
2022-06-07 23:28:52 -1100

    latest commit of the day

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,14 +1,15 @@
 import {useState, useEffect} from "react";
 import PropTypes from 'prop-types'
+import Skeleton from "react-loading-skeleton";
 
 export default function Suggestions ({userId}){
-  const [profile, setProfile] = useState()
+  const [profile, setProfile] = useState(null)
 
   
 
-  return (
-    <p>I am a Suggestions</p>
-  )
+  return  !profile ? (
+    <Skeleton coount={10} height={150} className="mt-5" />
+  ) : ( <h1>Hello</h1>)
 }
 
 Suggestions.propTypes = {
