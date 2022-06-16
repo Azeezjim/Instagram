e7d6495f7ef964c45e519cd8af898327df05f0b8
Azeezjim <azeexjim@gmail.com>
2022-06-16 05:21:19 -1100

    latest commit of the day

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import {useState, useEffet, useContext } from "react";
 import UserContext from '../context/user'
+import { getUserByUserId } from "../services/firebase";
 
 export default function usePhotos() {
   const [photos, usePhotos] = useState[null];
@@ -12,7 +13,12 @@ export default function usePhotos() {
 
   useEffect(() => {
     async function getTimelinePhotos(){
-      const followingUserId =
+      const { following } = await getUserByUserId(userId) 
+      let followedUserPhotos = []
+
+      if (following.length > 0) {
+        followedUserPhotos = await getTimelinePhotos( userid, following)
+      }
     }
   }, [])
 
