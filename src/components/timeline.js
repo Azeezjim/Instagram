53f79b1342126d83d884d1b7a61bea6e0037c6d0
Azeezjim <azeexjim@gmail.com>
2022-06-26 22:04:30 -1100

    latestv commit of the day

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -9,12 +9,12 @@ export default function Timeline() {
     <div className="container col-span-2">
       {!photos ? (
         <>
-          {[ ...new Array(4)].map((_, index => 
+          {[ ...new Array(4)].map((_, index) => (
             <Skeleton key={index} count={1} width={320} height={400} />
           ))}
         </>
       ) :  photos?.length > 0 ? (
-        photos.map((content) => <p key={content.docId}>{content.imageSrc</p>)
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
       ) :(
         <p className="text-center text-2xl"> Follow people to see photos</p>
       )}
