edbbf5318f748ff177ddeb5eefc5b7c963a7c549
Azeezjim <azeexjim@gmail.com>
2022-06-26 21:12:46 -1100

    latestv commit of the day

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,10 +2,20 @@ import Skeleton from "react-loading-skeleton";
 import usePhotos from "../hooks/use-photos";
 
 export default function Timeline() {
-  const { photos } = usePhotos(); 
+  const { photos } = usePhotos();
+  
+  console.log('Photos', photos)
   return (
     <div className="container col-span-2">
-      <p>I i'm the timeline </p>
+      {!photos ? (
+        <>
+          {[ ...new Array(4)].map(item => 
+            <Skeleton key={i}>
+
+            </Skeleton>
+          )}
+        </>
+      )}
     </div>
   )
 }
