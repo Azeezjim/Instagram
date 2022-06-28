aa025c49981fb3e25c5640c0480a58995643bf36
Azeezjim <azeexjim@gmail.com>
2022-06-28 05:59:18 -1100

    latestv commit of the day

---
+++ src/components/post/inddex.js
@@ -0,0 +1,21 @@
+import { useRef } from 'react'
+import PropTypes from 'prop-types'
+
+export default function Post ({ contnt }) {
+  return <p>I am a post</p>
+}
+
+Post.PropTypes = {
+  content : PropTypes.shape({
+    username : PropTypes.string.isRequired,
+    imageSrc : PropTypes.string.isRequired,
+    caption : PropTypes.string.isRequired,
+    docId : PropTypes.string.isRequired,
+    userLikedPhoto : PropTypes.bool.isRequired,
+    likes  : PropTypes.array.isRequired,
+    comment  : PropTypes.array.isRequired,
+    dataCreted  : PropTypes.number.isRequired
+    
+
+  })
+}
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,6 @@
 import Skeleton from "react-loading-skeleton";
 import usePhotos from "../hooks/use-photos";
+import Post from "./post/inddex";
 
 export default function Timeline() {
   const { photos } = usePhotos();
@@ -8,13 +9,11 @@ export default function Timeline() {
   return (
     <div className="container col-span-2">
       {!photos ? (
-        <>
-          {[ ...new Array(4)].map((_, index) => 
-            <Skeleton key={index} count={1} width={640} height={600} className='mb-5'/>
-          )}
-        </>
+            <Skeleton count={4} width={640} height={600} className='mb-5'/>
       ) :  photos?.length > 0 ? (
-        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+        photos.map((content) => <Post key={content.docId} conten={content} />
+        
+        )
       ) :(
         <p className="text-center text-2xl"> Follow people to see photos</p>
       )}
